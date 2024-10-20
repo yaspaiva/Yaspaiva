0983fad9f5e92e2fbc5eedc78a6998266eb7674c
yaspaiva <y.paiva@escolar.ifrn.edu.br>
2024-10-19 23:24:32 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ src/test/java/ifrn/pi/eventos/controllers/indexController.java
@@ -0,0 +1,16 @@
+package ifrn.pi.eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+@RequestMapping("/index")
+public String index() {
+	return "home";
+}
+
+	
+	
+	
+}
