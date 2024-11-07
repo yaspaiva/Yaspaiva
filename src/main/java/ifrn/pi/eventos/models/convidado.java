342fc9b4db0a65857ef4ca4225e4cc28c5a7e546
YASMIM MOREIRA DE PAIVA <20211204010010@ifrn.local>
2024-11-07 15:11:54 -0300

    tentando acertar

---
+++ src/main/java/ifrn/pi/eventos/models/convidado.java
@@ -0,0 +1,78 @@
+package ifrn.pi.eventos.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class convidado {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String rg;
+	
+	@ManyToOne
+	private Evento evento;
+
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public String getRg() {
+		return rg;
+	}
+
+
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+
+
+	public Evento getEvento() {
+		return evento;
+	}
+
+
+
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "convidado [id=" + id + ", nome=" + nome + ", rg=" + rg + ", evento=" + evento + ", getClass()="
+				+ getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+	
+
+}
